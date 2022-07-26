f0404fc8b226f05a70d17242de0776bdc32f20c3
quassbottle <quassbottleyt@gmail.com>
2022-07-27 01:08:42 +0300

    initial commit

---
+++ BrainfuckDotNet/Brainfuck.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace BrainfuckDotNet
+{
+    public class Brainfuck
+    {
+        private int _dataSize;
+        public Brainfuck(int size = 30000)
+        {
+            _dataSize = size;
+        }
+        public void Run(string code)
+        {
+            int[] array = new int[_dataSize];
+            int p = array.Length / 2;
+            int b = 0;
+
+            for (int i = 0; i < code.Length; i++)
+            {
+                switch (code[i])
+                {
+                    case '>':
+                        p++;
+                        break;
+                    case '<':
+                        p--;
+                        break;
+                    case '+':
+                        array[p]++;
+                        break;
+                    case '-':
+                        array[p]--;
+                        break;
+                    case '.':
+                        Console.Write((char)array[p]);
+                        break;
+                    case ',':
+                        array[p] = Console.Read();
+                        break;
+                    case '[':
+                        if (array[p] == 0)
+                        {
+                            b++;
+                            while (b > 0)
+                            {
+                                i++;
+                                if (code[i] == '[')
+                                {
+                                    b++;
+                                }
+                                if (code[i] == ']')
+                                {
+                                    b--;
+                                }
+                            }
+                        }
+                        break;
+                    case ']':
+                        if (array[p] == 0)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            if (code[i] == ']')
+                            {
+                                b++;
+                            }
+                            while (b > 0)
+                            {
+                                i--;
+                                if (code[i] == '[')
+                                {
+                                    b--;
+                                }
+                                if (code[i] == ']')
+                                {
+                                    b++;
+                                }
+                            }
+                            i--;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+    }
---
+++ BrainfuckDotNetCompiler/Program.cs
@@ -0,0 +1,39 @@
+﻿using BrainfuckDotNet;
+
+namespace BrainfuckDotNetCompiler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "BrainfuckDotNetCompiler";
+            var b = new Brainfuck();
+            if (args.Length != 0)
+            {
+                if ((args[0] == "--path" || args[0] == "--p") && args.Length > 1)
+                {
+                    string path = args[1];
+                    using (var r = new StreamReader(path))
+                    {
+                        b.Run(r.ReadToEnd());
+                        Console.Read();
+                        return;
+                    }
+                }
+                else
+                {
+                    string text = String.Join("", args);
+                    b.Run(text);
+                    Console.Read();
+                    return;
+                }
+            }
+            Console.WriteLine("Enter the Brainfuck code -> press enter -> then press Ctrl+Z (Ctrl+D for Linux users) -> enter");
+            Console.WriteLine("Also you can specify the path with the code using the --path");
+            Console.WriteLine("You can find all the information in the repository: https://github.com/quassbottle/BrainfuckDotNet/");
+            Console.WriteLine("=========");
+            b.Run(Console.In.ReadToEnd());
+            Console.Read();
+        }
+    }
+}
